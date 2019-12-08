0a62b75f1059effdbde5e0597e794161a2da6ec2
魏俊杰 <kolewei@163.com>
2019-12-08 20:43:32 +0800

    ✨ add web
    添加web

---
+++ src/main/java/tt/kole/demo/web/HomeController.java
@@ -0,0 +1,23 @@
+package tt.kole.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Create With: checkline
+ * Create By: kole
+ * Date: 2019/12/8
+ */
+@RestController()
+public class HomeController {
+
+    private String appName = "home";
+
+    @GetMapping("/index")
+    public String index(String name) {
+        return this.appName + "  ??  " +  name + " = kole =" + (new Date()).toLocaleString();
+    }
+
+}
